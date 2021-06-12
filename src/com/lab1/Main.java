e390a655f51572a73e120af66cbf29d968e4bd8b
Zielonyy3 <poprostuzielinski@gmail.com>
2021-06-12 17:48:48 +0200

    Zadanie 1 - Hello World

---
+++ src/com/lab1/Main.java
@@ -0,0 +1,13 @@
+package com.lab1;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(Arrays.toString(args));
+        System.out.println(new Date());
+    }
+}
